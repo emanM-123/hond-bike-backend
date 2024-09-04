42d753efec9696491f0d21192aab32854f77653c
emanM-123 <emanmaharana1995@gmail.com>
2024-09-04 20:19:54 +0530

    Initial commit

--- src/controller/emailservice.js
+++ src/controller/emailservice.js
@@ -3,10 +3,23 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 const { google } = require('googleapis');
-const credentials = require('../../cridentials.json'); 
+require('dotenv').config();
 
 const auth = new google.auth.GoogleAuth({
-    credentials,
+    credentials: {
+        type: process.env.TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+        universe_domain: process.env.UNIVERSE_DOMAIN
+
+    },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
